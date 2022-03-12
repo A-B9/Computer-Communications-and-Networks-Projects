fae226d15267ac8e0e1bf07e252b11e284beb8e9
arbnorbregu <arbnor.bregu@gmail.com>
2022-03-12 21:01:29 +0000

    Update, Receiver and Sender are almost complete.

--- COMN-CW/CW2/Receiver1.py
+++ COMN-CW/CW2/Receiver1.py
@@ -4,8 +4,41 @@ from socket import *
 import sys
 import os
 
-BUFF_SIZE = 1024 #same as payload size
+BUFF_SIZE = 1027 #same as payload size + header size 
 
-PORT, FILENAME = os.path.basename(sys.argv[1:-1])
-#PORT = port number which the receiver shall use to recevie messages
-#FILENAME = name to use for the recevied file, save the file with this name on local disk. 
+def main(argv):
+
+    PORT = int(os.path.basename(argv[1]))
+    FILENAME = os.path.basename(argv[2])
+    #PORT = port number which the receiver shall use to recevie messages
+    #FILENAME = name to use for the recevied file, save the file with this name on local disk. 
+
+    receiver_socket = socket(AF_INET, SOCK_DGRAM)
+    receiver_socket.bind(('', PORT))
+
+    file = open(FILENAME, 'wb+')
+
+    received_packet = receiver_socket.recvfrom(BUFF_SIZE)
+
+    while (received_packet):
+
+        received_packet_bytes = bytearray(received_packet[0])
+
+        sequence_number = received_packet_bytes[0:2]
+        #sequence_number = int.from_bytes(sequence_number, 'big')
+        end_of_file = received_packet_bytes[2]
+
+        payload_data = received_packet_bytes[3:]
+
+        if end_of_file == 1:
+            file.write(payload_data)
+            file.close
+            print('<FILE HAS BEEN TRANSFERRED CORRECTLY>')
+            break
+
+        file.write(payload_data)
+
+        received_packet = receiver_socket.recvfrom(BUFF_SIZE)
+
+if __name__ == "__main__":
+    main(sys.argv)
--- COMN-CW/CW2/Sender1.py
+++ COMN-CW/CW2/Sender1.py
@@ -4,16 +4,43 @@ from socket import *
 import sys
 import os
 
-BUFF_SIZE = 1024 #equivalent to payload length
 
-HOST_NAME, HOST_PORT, FILENAME = os.path.basename(sys.argv[1:-1])
-#HOST = IP address or host name for the receiver
-#PORT = port number of the receiver
-#FILENAME = the file to transfer to the receiver
+def main(argv):
 
-BANDWIDTH = 10 #bandwidth is 10Mbps
-PROP_DELAY = 5 #propogation delay = 5ms (1 way delay), 0% packet loss
+    BUFF_SIZE = 1024 #equivalent to payload length
+    HEADER_SIZE = 3
 
-senderSocket = socket(AF_INET, SOCK_DGRAM)
+    HOST_NAME = os.path.basename(argv[1])
+    HOST_PORT = int(os.path.basename(argv[2]))
+    FILE = os.path.basename(argv[3])
 
-#senderSocket.sendto(fileToSend.encode(), (HOST_NAME, HOST_PORT))
+    senderSocket = socket(AF_INET, SOCK_DGRAM)
+
+    file = open(FILE, 'rb') #read the file (store it in file). 'r' means we read the file, 'b' means binary mode such as images.
+    read_file = file.read(BUFF_SIZE)
+    packet_no = 0
+
+    while (read_file):
+        #if eof = 0, not the end of the file
+        #if eof = 1, it is the end of the file
+        end_of_file = (0).to_bytes(1, 'big')
+
+        if (len(read_file) < BUFF_SIZE):
+            end_of_file = (1).to_bytes(1, 'big')
+        
+        sequence_number = packet_no.to_bytes(2, 'big')
+        
+        file_to_send = bytearray()
+        file_to_send[0:0] = sequence_number
+        file_to_send[2:2] = end_of_file
+        file_to_send[3:3] = bytearray(read_file)
+
+        senderSocket.sendto(file_to_send, (HOST_NAME, HOST_PORT))
+
+        read_file = file.read(BUFF_SIZE)
+        packet_no += 1
+    
+    file.close()
+
+if __name__ == "__main__":
+    main(sys.argv)
