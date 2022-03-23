a9c77cc06c89d633aa82fc66f4c4c48cc4fc0c9d
arbnorbregu <arbnor.bregu@gmail.com>
2022-03-23 16:48:25 +0000

    udpate version 2

--- COMN-CW/CW2/Receiver2.py
+++ COMN-CW/CW2/Receiver2.py
@@ -20,65 +20,50 @@ def main(argv):
     file = open(FILENAME, 'wb+')
 
     #receive a packet from the socket, store the payload and sender address
-    received_packet, sender_address = receiver_socket.recvfrom(BUFF_SIZE)
+    recv = receiver_socket.recvfrom(BUFF_SIZE)
 
     #empty array to hold the sequence numbers of previously received packets
     last_sequence = None
 
     packets_received = 0
-    total_acks_sent = 0
 
-
-    print("<READY TO RECEIVE PACKETS>")
-    while (received_packet):
-
-        packets_received += 1
-
-        #received_packet = bytearray(BUFF_SIZE)
-        sequence_number_bytes = received_packet[0:2]
-        end_of_file = received_packet[2:3]
-        payload_data = received_packet[3:]
+    while (recv):
         
+        received_packet = recv[0]
+        sender_address = recv[1]
+        
+        seq_num = received_packet[0:2]
+        eof = int.from_bytes(received_packet[2:3], byteorder='big')
+        payload = received_packet[3:]
 
-        ack = sequence_number_bytes #save the sequence number as ack
+        if seq_num == last_sequence:
+            incorrectAck = seq_num
+            receiver_socket.sendto(incorrectAck, sender_address)
+            continue
 
-
-        if ack != last_sequence: #if the packet is new
-            receiver_socket.sendto(ack, sender_address) #send the ack back to sender
-
-            total_acks_sent += 1
-            #end_of_file = int.from_bytes(end_of_file, 'big')
-
-            if end_of_file == (1).to_bytes(1, 'big'): #if it is the final packet for the file
-                file.write(payload_data)
-                file.close()
-                print("FILE HAS BEEN TRANSFERRED CORRECTLY")
-                
+        else:
+            if eof == 1:
+                packets_received += 1
+                last_sequence = seq_num
+                file.write(payload)
+                ack = seq_num
+                receiver_socket.sendto(ack, sender_address)
+                print("END O FILE")
                 break
-            #if its not the final packet do this
-            print("sequence number %d has been saved correctly"% packets_received)
-
-            file.write(payload_data)
-
-            last_sequence = ack #update the list of already received packets
-
-
-        else: #if the packet is a duplicate
+            
+            packets_received += 1
+            last_sequence = seq_num
+            file.write(payload)
+            ack = seq_num
             receiver_socket.sendto(ack, sender_address)
-            total_acks_sent += 1
-            continue #wait for the correct packet to be received.
 
-        received_packet = receiver_socket.recvfrom(BUFF_SIZE)[0] #receive the next packet
+            recv = receiver_socket.recvfrom(BUFF_SIZE)
 
-
-    print("<PROCESS COMPLETE>")
-    print("<TOTAL PACKETS RECEIVED %d>"%packets_received)
-    print("<TOTAL ACKS SENT %d>"%total_acks_sent)
-
-    file.close
+    print(packets_received)
+    file.close()
     receiver_socket.close()
-    
-    
+
+        
 
 if __name__ == '__main__':
     main(sys.argv)
--- COMN-CW/CW2/Sender2.py
+++ COMN-CW/CW2/Sender2.py
@@ -4,115 +4,125 @@ from socket import *
 import sys
 import os
 import time
-import select
 
 BUFF_SIZE = 1024 #Buffer size
 HEADER_SIZE = 3 #Size of header
 
-
-def milisecondTimer():
-
-    start_time = time.time() * 1000 #1000 seconds in a milisecond. so *1000 to get current time in miliseconds.
-    while True: #loop forever
-        yield time.time() * 1000 - start_time #get the time difference
-
-
 def main(argv):
 
-
-
-
     #command line arguements
-    HOST_NAME = os.path.basename(argv[1])
-    HOST_PORT = int(os.path.basename(argv[2]))
+    HOST = os.path.basename(argv[1])
+    PORT = int(os.path.basename(argv[2]))
     FILE = os.path.basename(argv[3])
-    RETRY_TIMEOUT = int(os.path.basename(argv[4])) #in miliseconds
+    TIMEOUT = int(os.path.basename(argv[4])) #in miliseconds
 
 
     sender_socket = socket(AF_INET, SOCK_DGRAM) #create socket
     file = open(FILE, 'rb') #open the file to be read
     read_file = file.read(BUFF_SIZE) #read the file in chunks of size 1024
+
     packet_no = 0 #keep track of the packet being sent
+    eof = (0).to_bytes(1, byteorder='big')
 
     re_transmit = 0 #Track retransmissions
-    total_transmissions = 0 #Track total transmissions
     file_size = 0 #Track the size of the file, needed to calculate throughput
 
+    start_time = time.time()
+    print(len(read_file))
+
+    total_packets =  round(len( bytearray( file.read()) ) / 1024)
+    print(total_packets)
+
+    receiveAck = False
+    correctAck = False
 
 
-
-
-
-    true_start = time.time() #Start the timer to calculate true runtime of the program
-
-    while (read_file): #While there are still chunks of data left 
-        end_of_file = (0).to_bytes(1, 'big') #EOF flag set to 0
-
-        if len(read_file) < BUFF_SIZE: #When the size of the is smaller than buffer size
-            end_of_file = (1).to_bytes(1, 'big') #Set eof flag to 0, indicates end of the file has been reached
-            print("<END OF FILE>")
-
-        sequence_number = packet_no.to_bytes(2, 'big') #store the sequence number of the packet
-
-        file_to_send = bytearray(BUFF_SIZE + HEADER_SIZE) #Create bytearray to store the data of the packet
-        file_to_send[0:2] = sequence_number #Store the sequence number, 2 bytes
-        file_to_send[2:3] = end_of_file #Store the eof flag, 1 byte
-        file_to_send[3:] = bytearray(read_file) #Store payload data
-        print("<SENDING PACKET %d>"%packet_no)
-
-
-        sender_socket.sendto(file_to_send, (HOST_NAME, HOST_PORT)) #Send to receiver
-        total_transmissions += 1 #When packet has been transmitted, increment total counter
-
-        socket_timer = milisecondTimer() #start countdown timer
-
-        ###### up to here everything is standard and should work perfect #########
-        while True:
-
-
-            wait_for_ack = select.select([sender_socket], [], [], 0)
-            wait_for_ack = wait_for_ack[0]
-
-            if wait_for_ack: #If the receiver contains a 2 byte packet 
-                print("<ISACK PACKET RECEIVED>")
-
-                ack = wait_for_ack[0].recvfrom(2)[0] #store the 2 byte packet as the ACK
-                ack = ack[0:2]
-                if sequence_number == ack: #if the ack is equal to the current sequence number
-                    #packet has been received  correctly 
-                    print("<PACKET %d DELIVERED>"%packet_no)
-                    break #break out of the loop to continue sending the next packet
-
-                else: #if the ack is not correct
-                    print("<INCORRECT ACK RECEIVED, WILL WAIT>") #does nothing
-                    sender_socket.sendto(file_to_send, (HOST_NAME, HOST_PORT))#Resend the packet
-                    re_transmit += 1
-                    total_transmissions += 1
-
-                    continue
-            else: #if no packets received
-                if next(socket_timer) >= RETRY_TIMEOUT: #if the timer is equal to or exceeds RETRY_TIMEOUT
-                
-                #from the rdt3.0 FSM we must resend the packet and start the timer again.
-
-                    sender_socket.sendto(file_to_send, (HOST_NAME, HOST_PORT))#Resend the packet
-                    re_transmit += 1
-                    total_transmissions += 1
-                    socket_timer = milisecondTimer()
-
-                    print("<RETRANSMISSION NUMBER: %d>>>>>>>>>>>>>>>>>>>>>>>"%re_transmit)
-                    continue
-
+    while(read_file):
         
-        file_size = file_size + len(read_file) #count the total bytes of the file sent so far
-        read_file = file.read(BUFF_SIZE) #read in the next 1024 bytes of the file
-        packet_no += 1 #increment the packet number to indicate the process is moving to the next packet
+        print('hello world')
 
-        time.sleep(0.005)
+        print("SEND PACKET %d"%packet_no)
+        seq_num = packet_no.to_bytes(2, byteorder='big')
+        payload = bytearray(read_file)
+
+        send_file = bytearray(HEADER_SIZE + BUFF_SIZE)
+        send_file[0:2] = seq_num
+        send_file[2:3] = eof
+        send_file[3:] = payload
+
+        sender_socket.sendto(send_file, (HOST, PORT))
 
 
-    total_time_taken = time.time() - true_start #calculate total runtime of the program once all packets sent
-    throughput = (file_size / total_time_taken) #calculate throughput
+        while (correctAck == False):
+            try:
+                sender_socket.settimeout(TIMEOUT * 0.001)
+                ack, sender_addr = sender_socket.recvfrom(2)
+                ack = ack[0:2]
+                receiveAck = True
+
+            except socket.timeout:
+                receiveAck = False
+
+            if receiveAck == True:
+                if ack == seq_num:
+                    correctAck = True
+                    print("CORRECT ACK")
+                    break
+            else:
+                sender_socket.sendto(send_file, (HOST, PORT))
+                re_transmit += 1
+        
+        print("update read_file")
+        file_size += len(read_file)
+        print('file size %d'%file_size)
+        
+        packet_no += 1
+
+        read_file = file.read(BUFF_SIZE)
+
+        print(len(read_file))
+
+    print('length of file is smaller than buff_size')
+    print(len(read_file))
+
+    if (len(read_file) < BUFF_SIZE):
+        print('FINAL PACKET')
+        print("SEND PACKET %d"%packet_no)
+        seq_num = packet_no.to_bytes(2, byteorder='big')
+        eof = (1).to_bytes(1, byteorder='big')
+        payload = bytearray(read_file)
+
+        send_file = bytearray(HEADER_SIZE + BUFF_SIZE)
+        send_file[0:2] = seq_num
+        send_file[2:3] = eof
+        send_file[3:] = payload
+
+        sender_socket.sendto(send_file, (HOST, PORT))
+
+        while (correctAck == False):
+            try:
+                sender_socket.settimeout(TIMEOUT * 0.001)
+                ack, sender_addr = sender_socket.recvfrom(2)
+                ack = ack[0:2]
+                receiveAck = True
+            except socket.timeout:
+                receiveAck = False
+            if receiveAck == True:
+                if ack == seq_num:
+                    correctAck = True
+                    print("CORRECT ACK")
+            else:
+                sender_socket.sendto(send_file, (HOST, PORT))
+                re_transmit += 1
+    
+        file_size += len(read_file)
+
+        packet_no += 1
+
+
+    print('i am here for whatever reason')
+    total_time_taken = time.time() - start_time #calculate total runtime of the program once all packets sent
+    throughput = round(file_size / total_time_taken) #calculate throughput
 
     print("Number of retransmissions: " +str(re_transmit) + ", Throughput: " + str(throughput)) #necessary output
 
